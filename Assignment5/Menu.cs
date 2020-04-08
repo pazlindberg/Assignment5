b02f8d357a7eeb817eb464ea769eca0c8a48ed2a
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-04-08 10:28:41 +0200

    ugh2

---
+++ Assignment5/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5
+{
+    public class MenuMap
+    {
+        public string Text { get; set; }
+        public Action ToInvoke { get; set; }
+
+        public MenuMap(string txt, Action toInvoke)
+        {
+            Text = txt;
+            ToInvoke = toInvoke;
+        }
+    }
+
+    public class Menu
+    {
+        public string Name { get; set; }
+        public Dictionary<char, MenuMap> menuOptions = new Dictionary<char, MenuMap>();
+        public int Count { get; set; }
+
+
+        public Menu(string name, Dictionary<char, MenuMap> mOpts)
+        {
+            this.Name = name;
+            foreach (var ch in mOpts.Keys)
+            {
+                menuOptions.Add(ch, mOpts.GetValueOrDefault(ch));
+            }
+            this.Count = menuOptions.Count; //get/set?
+        }
+
+        public void Print()
+        {
+            //Console.Clear();
+            Console.WriteLine("\n\n\t{0}", Name);
+            foreach (var ch in menuOptions.Keys)
+            {
+                Console.WriteLine($"{ch} - {menuOptions.GetValueOrDefault(ch).Text}");
+            }
+        }
+    }
+}
--- Assignment5/UI.cs
+++ Assignment5/UI.cs
@@ -4,46 +4,7 @@ using System.Text;
 
 namespace Assignment5
 {
-    class MenuMap
-    {
-        public string Text { get; set; }
-        public Action ToInvoke { get; set; }
-        
-
-        public MenuMap(string txt, Action toInvoke)
-        {
-            Text = txt;
-            ToInvoke = toInvoke;
-        }
-    }
-
-    class Menu //normalt i egna filer mm
-    {
-        public string Name { get; set; }
-        public Dictionary<char, MenuMap> menuOptions = new Dictionary<char, MenuMap>();
-        public int Count { get; set; }
-        
-
-        public Menu(string name, Dictionary<char, MenuMap> mOpts)
-        {
-            this.Name = name;
-            foreach(var ch in mOpts.Keys)
-            {
-                menuOptions.Add(ch, mOpts.GetValueOrDefault(ch));
-            }
-            this.Count = menuOptions.Count; //get/set?
-        }
-
-        public void Print()
-        {
-            //Console.Clear();
-            Console.WriteLine("\n\n\t{0}",Name);
-            foreach(var ch in menuOptions.Keys)
-            {
-                Console.WriteLine($"{ch} - {menuOptions.GetValueOrDefault(ch).Text}");
-            }        
-        }
-    }
+    
 
     class UI
     {
@@ -273,7 +234,7 @@ namespace Assignment5
             }
         }
 
-            public bool Add()
+            private bool Add()
             {
                 //theGarage.Add();
                 string type = AskForType();
